b31b0ad71dc3f999af36dcb11deec712832e3279
Martin Garcia <newluxfero@gmail.com>
2026-02-06 00:03:41 +0100

    chore: update to v0.2.0

--- Formula/cali.rb
+++ Formula/cali.rb
@@ -1,15 +1,23 @@
 class Cali < Formula
   desc "Calendar CLI - View your calendar events in the terminal"
   homepage "https://github.com/magarcia/cali"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/magarcia/cali/releases/download/v#{version}/cali-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "456814b70d72e39c51259a96a882f5e2ed0c577d356dc22d78987b85bf49d66b"
+      sha256 "fef7f01028e99954993490aab613a981a77c00706da38375f752470cbe83de90"
     else
-      odie "Cali is currently only available for ARM64 (Apple Silicon) Macs."
+      url "https://github.com/magarcia/cali/releases/download/v#{version}/cali-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "dbf8423f60507f5aa1228b1978bd017920e91cd97378006bec9dafebcf24876b"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/magarcia/cali/releases/download/v#{version}/cali-#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 ""
     end
   end
 
